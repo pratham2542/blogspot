7b7c7b9af522cf3a0faef622d312026ce62b7817
Pratham <103320984+pratham2542@users.noreply.github.com>
2024-02-12 20:27:48 +0530

    Update index.jsx

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -58,19 +58,7 @@ const Navbar = () => {
         >
           Blogspot
         </Typography>
-        {/* {isNonMobileScreens && (
-          <FlexBetween
-            backgroundColor={neutralLight}
-            borderRadius="9px"
-            gap="3rem"
-            padding="0.1rem 1.5rem"
-          >
-            <InputBase placeholder="Search..." />
-            <IconButton>
-              <Search />
-            </IconButton>
-          </FlexBetween>
-        )} */}
+        
       </FlexBetween>
 
       {isNonMobileScreens ? (
